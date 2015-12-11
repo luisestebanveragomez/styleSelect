dade8790f720bcac727c030c322fa8c6dd36d88f
Luis Esteban Vera <estebanvera@iMac-de-Esteban.local>
2015-12-11 10:49:58 -0500

    add options

--- js/styleSelect.js
+++ js/styleSelect.js
@@ -4,23 +4,28 @@
  Github : https://github.com/kiokotzu/styleSelect
  Correo : esteban.vg@outlook.com
  Name Plugin: styleSelect.js
- version : 1.0.1
+ version : 1.0.2
  ---------------------------------------------*/
 
  $(function(){
-	jQuery.fn.styleSelect = function(){
+	jQuery.fn.styleSelect = function(settings){
+
+		var settingsDefault = jQuery.extend({
+			classSelect : 'select',
+			classArrow  : 'arrow'
+		}, settings);
 
 		this.each(function(){
 			var el =  $(this);
 
 			el.wrap(function() {
-			  return "<div class='select'></div>";
+			  return "<div class='" + settingsDefault.classSelect + "'></div>";
 			});
 
-			el.parent('.select').append("<div class='arrow'></div>");
+			el.parent('.' + settingsDefault.classSelect).append("<div class='" + settingsDefault.classArrow + "'></div>");
 
-			var textSelect = el.parent('.select').find('option[value=""]').text();
-			el.parent('.select').find('.arrow').text(textSelect);
+			var textSelect = el.parent('.' + settingsDefault.classSelect).find('option:selected').text();
+			el.parent('.' + settingsDefault.classSelect).find('.arrow').text(textSelect);
 
 			function select(q){
 				value = q.find('option:selected').text();
